a8a5414d7a6233035b648057d72a745de1eb13b3
ArinaD <ademkhina@list.ru>
2023-04-05 10:43:52 +0700

    Доки готовы

--- quadro.py
+++ quadro.py
@@ -22,6 +22,15 @@ IS_BORDERS = False
 class QuadTree:
     class QuadNode:
         def __init__(self, img: Image, x, y, width, height, color=None):
+            """
+            Класс элемента квадродерева
+            :param img: исходное изображение
+            :param x: координата x
+            :param y: координата y
+            :param width: ширина элемента
+            :param height: высота элемента
+            :param color: цвет элемента
+            """
             self.x = x
             self.y = y
 
@@ -33,9 +42,16 @@ class QuadTree:
             self.children = []
 
         def is_leaf(self):
+            """
+            Метод проверки элемента на наличие детей
+            :return: True, если элемент является листом, иначе False
+            """
             return not self.children
 
         def split_to_four(self):
+            """
+            Метод разделения элемента на 4 ребенка
+            """
             child_w, child_h = ceil(self.w / 2), ceil(self.h / 2)
             self.children.append(
                 QuadTree.QuadNode(self.image, self.x, self.y,
@@ -51,30 +67,58 @@ class QuadTree:
                                   child_h, child_w, child_h))
 
         def render(self, screen):
+            """
+            Метод отрисовки элемента
+            :param screen: окно pygame
+            """
             box_rect = pg.Rect(self.x, self.y, self.w, self.h)
             pg.draw.rect(screen, self.color, box_rect)
             if IS_BORDERS:
                 pg.draw.rect(screen, (0, 0, 0), box_rect, 1)
 
         def get_all_nodes(self):
+            """
+            Метод получения всех элементов после текущего
+            :return: массив из элементов
+            """
             all_nodes = [self]
             for child_node in self.children:
                 all_nodes.extend(child_node.get_all_nodes())
             return all_nodes
 
         def set_color(self, color):
+            """
+            Сеттер цвета элемента
+            :param color: цвет в формате RGB
+            """
             self.color = color
 
         def __repr__(self):
+            """
+            Метод для наглядного представления элементов
+            """
             return f"Node({self.x}, {self.y}, {self.w}, {self.h}, " \
                    f"color={self.color}"
 
     def __init__(self, img):
+        """
+        Класс квадро дерева
+        :param img: исходное изображение
+        """
         self.img = img
         self.root_node = QuadTree.QuadNode(img, 0, 0, *self.img.size)
 
     def build(self):
+        """
+        Метод построения дерева
+        """
         def build_inner(cur_node, cur_color, depth=0):
+            """
+            Построение отдельного элемента
+            :param cur_node: элемент
+            :param cur_color: цвет элемента
+            :param depth: текущая глубина
+            """
             lock = threading.Lock()
             if depth >= MAX_DEPTH:
                 with lock:
@@ -113,6 +157,9 @@ class QuadTree:
                                       self.root_node.h))
 
     def print_nodes(self):
+        """
+        Метод для вывода всех элементов дерева
+        """
         def _print_nodes(node: "QuadTree.QuadNode", depth=0):
             print("\t" * depth, node)
             for child in node.children:
@@ -122,12 +169,25 @@ class QuadTree:
         print("#" * 20)
 
     def render(self, screen):
+        """
+        Метод отрисовки листьев дерева
+        :param screen: окно pygame
+        """
         leaves = self.find_leaves()
         for leaf in leaves:
             leaf.render(screen)
 
     def find_leaves(self) -> List["QuadTree.QuadNode"]:
+        """
+        Метод для поиска листьев дерева
+        :return: все листья дерева
+        """
         def find_leaves_inner(cur_node: "QuadTree.QuadNode"):
+            """
+            Метод для нахождения листьев для переданного элемента
+            :param cur_node: элемент
+            :return: все листья
+            """
             if cur_node.is_leaf():
                 return [cur_node]
             else:
@@ -140,6 +200,12 @@ class QuadTree:
 
 
 def get_average_color(img, *box):
+    """
+    Метод получения среднего цвета элемента
+    :param img: исходное изображение
+    :param box: координаты элемента
+    :return: средний цвет
+    """
     region = img.crop(box)
     rg_array = np.array(region)
     rg_mean = cv2.mean(rg_array)[:-1]
@@ -147,6 +213,17 @@ def get_average_color(img, *box):
 
 
 def should_divide(img, cur_color, x, y, width, height):
+    """
+    Метод проверки необходимости делить элемент на детей
+    :param img: исходное изображение
+    :param cur_color: средний цвет элемента
+    :param x: координата х
+    :param y: координата у
+    :param width: ширина
+    :param height: высота
+    :return: True и средние цвета детей, если нужно и
+    False и средний цвет элемента, в ином случае
+    """
     avg_all = cur_color
     half_w, half_h = width / 2, height / 2
     avg_lt = get_average_color(img, x1 := x, y1 := y, x1 + half_w, y1 + half_h)
@@ -164,12 +241,20 @@ def should_divide(img, cur_color, x, y, width, height):
 
 
 def build_and_render(pic, screen):
+    """
+    Метод построения и отрисовки дерева
+    :param pic: исходное изображение
+    :param screen: окно pygame
+    """
     pic.build()
     pic.render(screen)
     pg.display.flip()
 
 
 def main():
+    """
+    Точка входа
+    """
     parser = argparse.ArgumentParser(description='QuadTree image processing')
     parser.add_argument('input_path', metavar='input_path', type=str,
                         help='Path to input image file')
@@ -205,7 +290,6 @@ def main():
                     # pg.image.save(screen, './images/image_new.jpg')
                     output_path = args.output_path or './images/image_new.jpg'
                     pg.image.save(screen, output_path)
-
 
 
 if __name__ == '__main__':
